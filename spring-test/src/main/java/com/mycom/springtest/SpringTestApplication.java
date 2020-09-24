101df99312e7e1952fa9d1378d401674e55afe65
songdalin <songdalin@qdcares.cn>
2020-09-24 22:08:21 +0800

    d

---
+++ spring-test/src/main/java/com/mycom/springtest/SpringTestApplication.java
@@ -0,0 +1,19 @@
+package com.mycom.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：songdalin
+ * @date ：2020/9/24 下午 10:05
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class SpringTestApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+}
